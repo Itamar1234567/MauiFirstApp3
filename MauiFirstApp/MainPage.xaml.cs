109d7521e655fb39e3715a80979ceac22bfa3d8f
User <User@LAB3-3>
2024-01-03 08:27:55 +0200

    Changes

---
+++ MauiFirstApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiFirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ChangeTextEvent(object sender, EventArgs e)
+        {
+            Button bt=(Button)sender;
+            bt.Text = "החזר טקסט";
+        }
+    }
+}
